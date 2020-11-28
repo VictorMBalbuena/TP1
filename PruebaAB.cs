3de5f37d15633733804c9d1cdad735849e40ec32
VictorMBalbuena <68522209+VictorMBalbuena@users.noreply.github.com>
2020-11-27 21:33:22 -0300

    Add files via upload

---
+++ PruebaAB.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace tp2
+{
+	class PruebaAB
+	{
+		public static void Main(string[] args)
+		{
+			ArbolBinario<int> arbolBinarioA = new ArbolBinario<int>(1);
+
+			ArbolBinario<int> hijoIzquierdo=new ArbolBinario<int>(2);
+			hijoIzquierdo.agregarHijoIzquierdo(new ArbolBinario<int>(3));
+			hijoIzquierdo.agregarHijoDerecho(new ArbolBinario<int>(4));
+			
+			ArbolBinario<int> hijoDerecho=new ArbolBinario<int>(5);
+			hijoDerecho.agregarHijoIzquierdo(new ArbolBinario<int>(6));
+			hijoDerecho.agregarHijoDerecho(new ArbolBinario<int>(7));
+			
+			arbolBinarioA.agregarHijoIzquierdo(hijoIzquierdo);
+			arbolBinarioA.agregarHijoDerecho(hijoDerecho);
+
+			arbolBinarioA.preorden();
+			arbolBinarioA.inorden();
+			arbolBinarioA.postorden();
+			arbolBinarioA.recorridoPorNiveles();
+			Console.Write("Presione una tecla para concluir la ejecucion . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
